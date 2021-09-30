a638e44d25d65f2fa54358fb40dd14f2ef30b500
Luis Castillo <mr.luiscastillo@gmail.com>
2021-09-29 22:39:58 -0400

    add controller poll action

--- mlb_voting_backend/app/controllers/polls_controller.rb
+++ mlb_voting_backend/app/controllers/polls_controller.rb
@@ -1,2 +1,26 @@
 class PollsController < ApplicationController
+
+    def create
+       poll = Poll.create(poll_params)
+       render json: poll 
+    end
+
+    def update
+        poll = Poll.find(params[:id])
+        poll.update(poll_params)
+        render json: poll
+    end
+
+    def destroy
+        poll = Poll.find(params[:id])
+        poll.destroy
+        render json: {}
+    end
+
+    private
+
+    def poll_params
+        params.require(:poll).permit(:question)
+    end
+
 end
