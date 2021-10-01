cc54ff429dea9d2f23ee32962f66fff4cb9c61b6
Luis Castillo <mr.luiscastillo@gmail.com>
2021-09-30 22:03:59 -0400

    import libraries to the root index.js

---
+++ mlb_voting_frontend/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extensions';
