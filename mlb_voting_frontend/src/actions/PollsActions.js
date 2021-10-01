7bd1d05d097e66fc4b63de2fd318cde816cf93b8
Luis Castillo <mr.luiscastillo@gmail.com>
2021-09-30 23:57:43 -0400

    add dispatch to pollsActions

--- mlb_voting_frontend/src/actions/PollsActions.js
+++ mlb_voting_frontend/src/actions/PollsActions.js
@@ -3,6 +3,6 @@ export const fetchPolls = () => {
     return (dispatch) => {
         fetch('http://localhost:3000/polls')
         .then(resp => resp.json())
-        .then(polls => console.log('fetchPolls', polls))
+        .then(polls => dispatch({ type: 'FETCH_POLLS', payload: polls}))
     }
 }
