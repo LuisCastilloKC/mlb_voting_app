516677d34268ff3470c742e4afd9187a171f478c
Luis Castillo <mr.luiscastillo@gmail.com>
2021-09-30 23:05:43 -0400

    add PollsContainer

---
+++ mlb_voting_frontend/src/components/PollsContainer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PollsContainer extends Component {
+    render(){
+        return (
+            <div>
+                Polls Container
+            </div>
+        );
+    }
+}
+
+export default PollsContainer;
