f858c175c289152ef1598f53d44edc16a2c2872d
Luis Castillo <mr.luiscastillo@gmail.com>
2021-10-03 22:45:34 -0400

    create PollsForm and add react component boiler plate

---
+++ mlb_voting_frontend/src/components/PollsForm.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class PollsForm extends Component {
+
+    state = {
+        question: ""
+    }
+
+
+    render(){
+        return (
+            <form>
+                
+            </form>
+        )
+    }
+}
+
+export default PollsForm;
