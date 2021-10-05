5515158b603e3bc6ce035743b124f5394327fce1
Luis Castillo <mr.luiscastillo@gmail.com>
2021-10-04 22:13:03 -0400

    add edit button

--- mlb_voting_frontend/src/components/Poll.jsx
+++ mlb_voting_frontend/src/components/Poll.jsx
@@ -2,21 +2,30 @@
 import React, { useState } from 'react';
 import PlayerName from './PlayerName'
 import PlayerForm from './PlayerForm'
-import { deletePoll } from '..actions/pollsActions'
+import { deletePoll } from '../actions/pollsActions'
+import { connect } from 'react-redux'
 
 const Poll = (props) => {
 
     const [showForm, setShowForm] = useState(false)
+    const [showEditForm, setShowEditForm] = useState(false)
 
     return(
         <>
             <h1>{props.pollKey.question}</h1>
             <button onClick={()=> setShowForm(!showForm)}>Add a Player</button>
             <button onClick={()=> props.deletePoll(props.pollKey.id)}>Delete Poll</button>
+            <button onClick={()=> setShowEditForm(!showEditForm)}>Edit Poll</button>
             {showForm && <PlayerForm poll_id={props.pollKey.id}/>}
             {props.pollKey.player_names.map(playerName => <PlayerName playerNameKey={playerName} />)}
         </>
     )
 }
 
-export default Poll;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        deletePoll: (pollId) => dispatch(deletePoll(pollId))
+    }
+}
+
+export default connect(null, mapDispatchToProps)(Poll);
