fdaa0433fb22becaa22136fd609fee4b69730373
Luis Castillo <mr.luiscastillo@gmail.com>
2021-10-04 22:18:51 -0400

    add editPollForm component

---
+++ mlb_voting_frontend/src/components/EditPollForm.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import { editPoll } from '../actions/pollsActions'
+import { connect } from 'react-redux'
+
+class EditPollForm extends Component {
+
+    state = {
+        question: this.props.poll.question,
+        id: this.props.poll.id
+    }
+
+    handleChange = (e) => {
+        const { name, value } = e.target
+
+        this.setState({ 
+            [name]: value
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault()
+        this.props.editPoll(this.state)
+    }
+
+    render(){
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>Edit Question:</label>
+                <input type="text" value={this.state.question} onChange={this.handleChange} name="question" />
+                <br />
+                <input type="submit" value="Update Poll" />
+            </form>
+        )
+    }
+}
+
+export default connect(null, { editPoll })(EditPollForm);
