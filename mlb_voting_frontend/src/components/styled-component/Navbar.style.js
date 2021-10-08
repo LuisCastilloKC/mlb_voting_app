2d235784cebecb69627b77ea67b845b676a75552
Luis Castillo <mr.luiscastillo@gmail.com>
2021-10-07 22:07:45 -0400

    styling the app

--- mlb_voting_frontend/src/components/styled-component/Navbar.style.js
+++ mlb_voting_frontend/src/components/styled-component/Navbar.style.js
@@ -1,6 +1,36 @@
 import styled from 'styled-components'
 
-const Navbar = styled.nav`
+// const Navbar = styled.nav`
+//     *{
+//         padding: 0;
+//         margin: 0;
+//         text-decoration: none;
+//         list-style: none;
+//         box-sizing: border-box;
+//     }
+//     body{
+//         font-family: montserrat;
+//     }
 
+//     nav{
+//         background: #0082e6;
+//         height: 80px;
+//         width: 100%;
+//     }
+//     label.logo{
+//         color: white;
+//         font-size: 35px;
+//         line-height: 80px;
+//         padding: 0 100px;
+//         font-weight: bold;
+//     }
+//     nav ul{
+//         float: right;
+//         margin-right: 20px;
+//     }
+//     nav ul li{
+//         display: inline-block;
+//         line-height: 80px;
+//     }
 
-`
+// `
