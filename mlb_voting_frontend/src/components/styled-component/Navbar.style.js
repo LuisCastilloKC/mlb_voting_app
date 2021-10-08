2d54c626dc2c1e1185529570cf380433b03e1f52
Luis Castillo <mr.luiscastillo@gmail.com>
2021-10-07 22:23:51 -0400

    add Nav styled

--- mlb_voting_frontend/src/components/styled-component/Navbar.style.js
+++ mlb_voting_frontend/src/components/styled-component/Navbar.style.js
@@ -1,4 +1,16 @@
 import styled from 'styled-components'
+import { NavLink as Link} from 'react-router-dom'
+
+
+export const Nav = styled.nav`
+background: #000;
+height: 80px;
+display: flex;
+justify-content: space-between;
+padding: 0.5rem cal((100vw - 1000px) / 2);
+z-index: 10;
+`
+
 
 // const Navbar = styled.nav`
 //     *{
