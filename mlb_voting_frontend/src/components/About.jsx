a9a43dac3a2c29b776dec465f9592aa3c0766e1f
Luis Castillo <mr.luiscastillo@gmail.com>
2021-10-09 01:21:56 -0400

    add text to about component

--- mlb_voting_frontend/src/components/About.jsx
+++ mlb_voting_frontend/src/components/About.jsx
@@ -2,6 +2,27 @@ import React from 'react';
 
 export const About = () => {
     return(
-        <h1> I'm the About Page</h1>
+        <>
+            <h1> About Baseball </h1>
+            <div>
+            <ul>Baseball is a bat-and-ball game played between two opposing teams, typically of nine players each, 
+                that take turns batting and fielding. The game proceeds when a player on the fielding team, called 
+                the pitcher, throws a ball which a player on the batting team tries to hit with a bat. The objective 
+                of the offensive team (batting team) is to hit the ball into the field of play, allowing its players 
+                to run the bases, having them advance counter-clockwise around four bases to score what are called "runs". 
+                The objective of the defensive team (fielding team) is to prevent batters from becoming runners, and to 
+                prevent runners' advance around the bases. A run is scored when a runner legally advances around the bases in order and touches home 
+                plate (the place where the player started as a batter). The team that scores the most runs by the end of the game is the winner.
+                
+                The first objective of the batting team is to have a player reach first base safely. A player on the batting team who reaches first 
+                base without being called "out" can attempt to advance to subsequent bases as a runner, either immediately or during teammates' 
+                turns batting. The fielding team tries to prevent runs by getting batters or runners "out", which forces them out of the field 
+                of play. Both the pitcher and fielders have methods of getting the batting team's players out. The opposing teams switch back a
+                nd forth between batting and fielding; the batting team's turn to bat is over once the fielding team records three outs. One turn 
+                batting for each team constitutes an inning. A game is usually composed of nine innings, and the team with the greater number of 
+                runs at the end of the game wins. If scores are tied at the end of nine innings, extra innings are usually played. Baseball has no 
+                game clock, although most games end in the ninth inning.</ul>
+                </div>
+        </>
     )
 }
--- mlb_voting_frontend/src/components/home.jsx
+++ mlb_voting_frontend/src/components/home.jsx
@@ -3,5 +3,6 @@ import React from 'react';
 export const Home = () => {
     return(
         <h1> I'm the Home Page</h1>
+        
     )
 }
