6ce5f0e622055d02a1e8a5db9689582bb4f99e8b
Luis Castillo <mr.luiscastillo@gmail.com>
2021-10-09 21:23:05 -0400

    fix typo in home page

--- mlb_voting_frontend/src/components/home.jsx
+++ mlb_voting_frontend/src/components/home.jsx
@@ -16,7 +16,7 @@ export const Home = () => {
             <br />
             <h1> Welcome to MLB Poll App</h1>
             <br />
-            <h3>In this App you will be bale to Create Poll and Add Player to that Poll </h3>
+            <h3>In this App you can Create Poll and Add Players to that Poll </h3>
             <h3>You will be bale to vote for each individual Player from any Poll </h3>
             
         </center>
