dd65accc808d95e41c89830f70e36da28500e3fb
Luis Castillo <mr.luiscastillo@gmail.com>
2021-10-09 20:59:26 -0400

    add pictures of players and styled

--- mlb_voting_frontend/src/components/TopPlayers.jsx
+++ mlb_voting_frontend/src/components/TopPlayers.jsx
@@ -1,10 +1,58 @@
 import React from 'react';
+import { TopPlayerWrapper } from './styled-component/Card.style'
 
 export const TopPlayers = () => {
     return(
-        
-            <h1>Top Baseball Player Picture</h1>
-            
-       
+        <>
+            <center><h1>Top Baseball Player Picture</h1></center>
+            <TopPlayerWrapper>
+                <h3>Vladimir Guerrero Jr.</h3> 
+                <img src="https://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/35002.png" alt="Vladimir Guerrero Jr." width="300" ></img>
+            </TopPlayerWrapper>
+            <TopPlayerWrapper>
+                <h3>Shohei Ohtani</h3> 
+                <img src="https://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/39832.png" alt="Shohei Ohtani" width="300" ></img>
+            </TopPlayerWrapper>
+            <TopPlayerWrapper>
+                <h3>Pete Alonso</h3> 
+                <img src="https://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/37498.png" alt="Pete Alonso" width="300" ></img>
+            </TopPlayerWrapper>
+            <TopPlayerWrapper>
+                <h3>Fernando Tatís Jr.</h3> 
+                <img src="https://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/35983.png" alt="Fernando Tatís Jr." width="300" ></img>
+            </TopPlayerWrapper>
+            <TopPlayerWrapper>
+                <h3>Bryce Harper</h3> 
+                <img src="https://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/30951.png" alt="Bryce Harper" width="300" ></img>
+            </TopPlayerWrapper>
+            <TopPlayerWrapper>
+                <h3>Salvador Pérez</h3> 
+                <img src="https://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/31127.png" alt="Salvador Pérez" width="300" ></img>
+            </TopPlayerWrapper>
+            <TopPlayerWrapper>
+                <h3>Albert Pujols</h3> 
+                <img src="https://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/4574.png" alt="Albert Pujols" width="300" ></img>
+            </TopPlayerWrapper>
+            <TopPlayerWrapper>
+                <h3>Mike Trout</h3> 
+                <img src="https://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/30836.png" alt="Mike Trout" width="300" ></img>
+            </TopPlayerWrapper>
+            <TopPlayerWrapper>
+                <h3>Manny Machado</h3> 
+                <img src="https://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/31097.png" alt="Manny Machado" width="300" ></img>
+            </TopPlayerWrapper>
+            <TopPlayerWrapper>
+                <h3>Nelson Cruz</h3> 
+                <img src="https://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/6242.png" alt="Nelson Cruz" width="300" ></img>
+            </TopPlayerWrapper>
+            <TopPlayerWrapper>
+                <h3>Randy Arozarena</h3> 
+                <img src="https://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/36488.png" alt="Randy Arozarena" width="300" ></img>
+            </TopPlayerWrapper>
+            <TopPlayerWrapper>
+                <h3>Aaron Judge</h3> 
+                <img src="https://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/33192.png" alt="Aaron Judge" width="300" ></img>
+            </TopPlayerWrapper>
+       </>
     )
 }
--- mlb_voting_frontend/src/components/styled-component/card.style.js
+++ mlb_voting_frontend/src/components/styled-component/card.style.js
@@ -16,3 +16,19 @@ export const Wrapper = styled.div`
   margin: 1rem;  
   }
 `;
+
+export const TopPlayerWrapper = styled.div`
+  display: inline-flex;
+  flex-wrap: center;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  width: 250px;
+  height: 260px;
+  border-radius: 4px;
+  border: 1px solid rgba(0, 0, 0, 0.125);
+  border-radius: 0.25rem;
+  padding: 1rem;
+  margin: 1rem;  
+  }
+`;
