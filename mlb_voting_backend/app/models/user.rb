9846c1a26daf745f7a8d51a72272ed83f86e1a71
Luis Castillo <mr.luiscastillo@gmail.com>
2021-12-21 23:49:50 -0500

    add validation for user

--- mlb_voting_backend/app/models/user.rb
+++ mlb_voting_backend/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
     # has secure password come from Bcrypt to have a secure password for user
     has_secure_password
+    validates :username, uniquess: { case_sensitive: false }
 end
