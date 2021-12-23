5473714932a6dbba768baaffca085e951e8047e9
Luis Castillo <mr.luiscastillo@gmail.com>
2021-12-22 23:10:05 -0500

    add on create acion creation of user using serializer

--- mlb_voting_backend/app/controllers/users_controller.rb
+++ mlb_voting_backend/app/controllers/users_controller.rb
@@ -6,7 +6,14 @@ class UsersController < ApplicationController
 
     def create
         @user = User.create(user_params)
-        render json: @user, status: :created
+        if @user.valid?
+            render json: { user: UserSerializer.new(@user) }, status: :created
+        else 
+            render json: {
+                error: 'fail to create user'
+            },
+            status: :unprocessable_entity
+        end
     end
 
     private
