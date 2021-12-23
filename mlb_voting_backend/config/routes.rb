8579bbf56a4f487231f20f6a442b6769e5bcdd93
Luis Castillo <mr.luiscastillo@gmail.com>
2021-12-22 23:16:34 -0500

    add create route only and route for login and profile for user

--- mlb_voting_backend/config/routes.rb
+++ mlb_voting_backend/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: [:create]
+  post '/login', to: 'auth#create'
+  get '/profile', to: 'user#profile'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 resources :polls, only: [:index, :create, :update, :destroy]
 resources :player_names, only: [:create, :update, :destroy]
