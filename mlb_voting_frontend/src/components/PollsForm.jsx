f5fffb6c4f5c63abedb83a85d939fb892d550c35
Luis Castillo <mr.luiscastillo@gmail.com>
2022-08-27 23:50:24 -0400

    refactor code  -  create funcion compoent, add useState hook

--- mlb_voting_frontend/src/components/PollsForm.jsx
+++ mlb_voting_frontend/src/components/PollsForm.jsx
@@ -1,49 +1,63 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { addPoll } from '../actions/PollsActions'
 import { connect } from 'react-redux'
 import {H2Form, FormWrapper, InputQuestion, StyledForm, StyledButton, ButtonDiv } from './styled-component/PollQuestionForm.style'
 
-class PollsForm extends Component {
 
-    state = {
+const PollsForm = () => {
+    const [question, setQuestion] = useState({
         question: ""
-    }
+    })
 
-    handleChange = (e) => {
-        const { name, value } = e.target
-
-        this.setState({ 
-            [name]: value
-        })
-    }
-
-    handleSubmit = e => {
-        e.preventDefault()
-        this.props.addPoll(this.state)
-        this.setState({
-            question: ""
-        })
-    }
-
-    
-    
-    render(){
-        return (
-
-    <FormWrapper>
-        <StyledForm onSubmit={this.handleSubmit} >
-            <H2Form>Create Poll</H2Form>
-            <InputQuestion type="text" value={this.state.question} onChange={this.handleChange} name="question" />
-            <ButtonDiv>
-                <StyledButton type="submit">CREATE</StyledButton> 
-            </ButtonDiv>
-            
-        </StyledForm>
-
-    </FormWrapper>
-
-        )
-    }
+    return(
+       <div></div>
+    )
 }
 
-export default connect(null, { addPoll })(PollsForm);
+export default PollsForm
+
+
+// class PollsForm extends Component {
+
+//     state = {
+//         question: ""
+//     }
+
+//     handleChange = (e) => {
+//         const { name, value } = e.target
+
+//         this.setState({ 
+//             [name]: value
+//         })
+//     }
+
+//     handleSubmit = e => {
+//         e.preventDefault()
+//         this.props.addPoll(this.state)
+//         this.setState({
+//             question: ""
+//         })
+//     }
+
+    
+    
+//     render(){
+//         return (
+
+//     <FormWrapper>
+//         <StyledForm onSubmit={this.handleSubmit} >
+//             <H2Form>Create Poll</H2Form>
+//             <InputQuestion type="text" value={this.state.question} onChange={this.handleChange} name="question" />
+//             <ButtonDiv>
+//                 <StyledButton type="submit">CREATE</StyledButton> 
+//             </ButtonDiv>
+            
+//         </StyledForm>
+
+//     </FormWrapper>
+
+//         )
+//     }
+// }
+
+// export default connect(null, { addPoll })(PollsForm);
