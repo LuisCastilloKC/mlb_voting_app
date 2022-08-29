e38d0527f8889be0c3f13bdfcc8288375753d006
Luis Castillo <mr.luiscastillo@gmail.com>
2022-08-28 23:45:54 -0400

    start refactoring Playerform to function component and add useState hook

--- mlb_voting_frontend/src/components/PlayerForm.jsx
+++ mlb_voting_frontend/src/components/PlayerForm.jsx
@@ -1,42 +1,56 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { addPlayer } from '../actions/playerNamesActions'
 import { connect } from 'react-redux'
 
-class PlayerForm extends Component {
 
-    state = {
+const PlayerForm = ({poll_id}) => {
+    const [playerName, setPlayerName] = useState({
         name: "",
-        poll_id: this.props.poll_id
-    }
+        poll_id: poll_id
+    })
 
-    handleChange = (e) => {
-        const { name, value } = e.target
+    return(<div>
 
-        this.setState({ 
-            [name]: value
-        })
-    }
-    
-    handleSubmit = e => {
-        e.preventDefault()
-        this.props.addPlayer(this.state)
-        this.setState({
-            name: ""
-        })
-    }
-
-    
-
-    render(){
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <label>Player Name:</label>
-                <input type="text" value={this.state.name} onChange={this.handleChange} name="name" />
-                <br />
-                <input type="submit" value="Create Player" />
-            </form>
-        )
-    }
+    </div>)
 }
 
-export default connect(null, { addPlayer })(PlayerForm);
+export default PlayerForm
+
+// class PlayerForm extends Component {
+
+//     state = {
+//         name: "",
+//         poll_id: this.props.poll_id
+//     }
+
+//     handleChange = (e) => {
+//         const { name, value } = e.target
+
+//         this.setState({ 
+//             [name]: value
+//         })
+//     }
+    
+//     handleSubmit = e => {
+//         e.preventDefault()
+//         this.props.addPlayer(this.state)
+//         this.setState({
+//             name: ""
+//         })
+//     }
+
+    
+
+//     render(){
+//         return (
+//             <form onSubmit={this.handleSubmit}>
+//                 <label>Player Name:</label>
+//                 <input type="text" value={this.state.name} onChange={this.handleChange} name="name" />
+//                 <br />
+//                 <input type="submit" value="Create Player" />
+//             </form>
+//         )
+//     }
+// }
+
+// export default connect(null, { addPlayer })(PlayerForm);
