05ed3008d2057f972992f540176e80472d4af9fa
Luis Castillo <mr.luiscastillo@gmail.com>
2022-08-29 00:12:12 -0400

    remove commentted class component

--- mlb_voting_frontend/src/components/PlayerForm.jsx
+++ mlb_voting_frontend/src/components/PlayerForm.jsx
@@ -8,15 +8,15 @@ const PlayerForm = ({ poll_id }) => {
 
   const [playerName, setPlayerName] = useState({
     name: "",
-    poll_id: poll_id,
+    poll_id: poll_id
   });
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
+    const {name, value} = e.target;
 
     setPlayerName({
       ...playerName,
-      [name]: value,
+      [name]: value
     });
   };
 
@@ -46,39 +46,3 @@ const PlayerForm = ({ poll_id }) => {
 
 export default PlayerForm;
 
-// class PlayerForm extends Component {
-
-//     state = {
-//         name: "",
-//         poll_id: this.props.poll_id
-//     }
-
-//     handleChange = (e) => {
-//         const { name, value } = e.target
-
-//         this.setState({
-//             [name]: value
-//         })
-//     }
-
-//     handleSubmit = e => {
-//         e.preventDefault()
-//         this.props.addPlayer(this.state)
-//         this.setState({
-//             name: ""
-//         })
-//     }
-
-//     render(){
-//         return (
-//             <form onSubmit={this.handleSubmit}>
-//                 <label>Player Name:</label>
-//                 <input type="text" value={this.state.name} onChange={this.handleChange} name="name" />
-//                 <br />
-//                 <input type="submit" value="Create Player" />
-//             </form>
-//         )
-//     }
-// }
-
-// export default connect(null, { addPlayer })(PlayerForm);
