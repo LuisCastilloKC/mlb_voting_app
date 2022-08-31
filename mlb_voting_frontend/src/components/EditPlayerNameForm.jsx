db00938228694c43fd4c6788209d04d56bbe8998
Luis Castillo <mr.luiscastillo@gmail.com>
2022-08-30 23:56:10 -0400

    initialize refactor for EditPlayerForm to functional component

--- mlb_voting_frontend/src/components/EditPlayerNameForm.jsx
+++ mlb_voting_frontend/src/components/EditPlayerNameForm.jsx
@@ -2,38 +2,53 @@ import React, { Component } from 'react'
 import { updatePlayerName } from '../actions/playerNamesActions'
 import { connect } from 'react-redux'
 
-class EditPlayerNameForm extends Component {
 
-    state = {
-        name: this.props.playerName.name,
-        poll_id: this.props.playerName.poll_id,
-        id: this.props.playerName.id
-    }
+const EditPlayerNameForm = (props) => {
+    const [playerNameObj, setPlayerNameObj] = useState({
+        name: props.playerName.name,
+        poll_id: props.playerName.poll_id,
+        id: props.playerName.id
+    })
 
-    handleChange = (e) => {
-        const { name, value } = e.target
-
-        this.setState({ 
-            [name]: value
-        })
-    }
-
-    handleSubmit = e => {
-        e.preventDefault()
-        this.props.updatePlayerName(this.state)
-        this.props.setShowForm(false)
-    }
-
-    render(){
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <label>Edit Name:</label>
-                <input type="text" value={this.state.name} onChange={this.handleChange} name="name" />
-                <br />
-                <input type="submit" value="Update Player" />
-            </form>
-        )
-    }
+    return(
+        <div></div>
+    )
 }
 
-export default connect(null, { updatePlayerName })(EditPlayerNameForm);
+export default EditPlayerNameForm
+
+// class EditPlayerNameForm extends Component {
+
+//     state = {
+//         name: this.props.playerName.name,
+//         poll_id: this.props.playerName.poll_id,
+//         id: this.props.playerName.id
+//     }
+
+//     handleChange = (e) => {
+//         const { name, value } = e.target
+
+//         this.setState({ 
+//             [name]: value
+//         })
+//     }
+
+//     handleSubmit = e => {
+//         e.preventDefault()
+//         this.props.updatePlayerName(this.state)
+//         this.props.setShowForm(false)
+//     }
+
+//     render(){
+//         return (
+//             <form onSubmit={this.handleSubmit}>
+//                 <label>Edit Name:</label>
+//                 <input type="text" value={this.state.name} onChange={this.handleChange} name="name" />
+//                 <br />
+//                 <input type="submit" value="Update Player" />
+//             </form>
+//         )
+//     }
+// }
+
+// export default connect(null, { updatePlayerName })(EditPlayerNameForm);
