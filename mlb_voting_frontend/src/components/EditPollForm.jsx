f059700fdee2b07ed56c201a24532e7ed1f71b88
Luis Castillo <mr.luiscastillo@gmail.com>
2022-09-01 23:59:39 -0400

    start poll component to functional component refactor

--- mlb_voting_frontend/src/components/EditPollForm.jsx
+++ mlb_voting_frontend/src/components/EditPollForm.jsx
@@ -1,38 +1,50 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { editPoll } from '../actions/PollsActions'
 import { connect } from 'react-redux'
 
-class EditPollForm extends Component {
 
-    state = {
-        question: this.props.poll.question,
-        id: this.props.poll.id
-    }
-
-    handleChange = (e) => {
-        const { name, value } = e.target
-
-        this.setState({ 
-            [name]: value
-        })
-    }
-
-    handleSubmit = e => {
-        e.preventDefault()
-        this.props.editPoll(this.state)
-        this.props.setShowEditForm(false)
-    }
-
-    render(){
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <label>Edit Question:</label>
-                <input type="text" value={this.state.question} onChange={this.handleChange} name="question" />
-                <br />
-                <input type="submit" value="Update Poll" />
-            </form>
-        )
-    }
+const EditPollForm = (props) => {
+    const [poll, setPoll] = useState({
+        question: props.poll.question,
+        id: props.poll.id
+    })
+    return(
+    <div></div>
+    )
 }
 
-export default connect(null, { editPoll })(EditPollForm);
+export default EditPollForm
+// class EditPollForm extends Component {
+
+//     state = {
+//         question: this.props.poll.question,
+//         id: this.props.poll.id
+//     }
+
+//     handleChange = (e) => {
+//         const { name, value } = e.target
+
+//         this.setState({ 
+//             [name]: value
+//         })
+//     }
+
+//     handleSubmit = e => {
+//         e.preventDefault()
+//         this.props.editPoll(this.state)
+//         this.props.setShowEditForm(false)
+//     }
+
+//     render(){
+//         return (
+//             <form onSubmit={this.handleSubmit}>
+//                 <label>Edit Question:</label>
+//                 <input type="text" value={this.state.question} onChange={this.handleChange} name="question" />
+//                 <br />
+//                 <input type="submit" value="Update Poll" />
+//             </form>
+//         )
+//     }
+// }
+
+// export default connect(null, { editPoll })(EditPollForm);
